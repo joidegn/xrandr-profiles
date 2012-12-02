1d256dbee0037d890afdf70e37fd5d231f706f4f
Johannes Degn <j@degn.de>
2012-12-02 12:21:10 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='xrandr-profiles',
+    version='0.0.1',
+    description='run xrandr with certain parameters on boot',
+    author='JoiDegn',
+    license="MIT",
+    url='https://github.com/JoiDegn/xrandr-profiles',
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "Operating System :: Unix",
+        "License :: OSI Approved :: MIT License",
+    ]
+
+)
